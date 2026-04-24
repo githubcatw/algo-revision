991c33c8c099c4b6a0d1464644b8a7b1e4d7de1b
githubcatw <17374742+githubcatw@users.noreply.github.com>
2026-04-24 20:55:01 +0400

    graph: add support for weighted graphs
    the weights are a part of the adjacency matrix in a weighted graph.
    to keep backwards compatibility with the old api weighted graphs
    are created and edited with new functions.

--- graph/graph.c
+++ graph/graph.c
@@ -10,6 +10,7 @@ Graph* create_graph(int size) {
     Graph* graph = (Graph*)malloc(sizeof(Graph));
     if (graph) {
         graph->size = size;
+        graph->weighted = 0;
         // initialize adjacency matrix
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
@@ -17,6 +18,13 @@ Graph* create_graph(int size) {
             }
         }
     }
+    return graph;
+}
+
+Graph* create_graph_weighted(int size) {
+    // create a graph and mark it as weighted
+    Graph* graph = create_graph(size);
+    graph->weighted = 1;
     return graph;
 }
 
@@ -35,4 +43,32 @@ void graph_connect(Graph* graph, int node_a, int node_b) {
     graph->adj_matrix[node_a][node_b] = 1;
     // since this is an impl of an undirected graph, create the opposite connection
     graph->adj_matrix[node_b][node_a] = 1;
+}
+
+void graph_connect_weighted(Graph* graph, int node_a, int node_b, int weight) {
+    // if this graph is not weighted, set the weight to 1
+    // (the lines at the end would make this function act like graph_connect this way)
+    if (!graph->weighted) {
+        printf("W: weight ignored as the graph is unweighted; use graph_connect for unweighted graphs or set graph->weighted = 1\n");
+        weight = 1;
+    }
+
+    // bound checking
+    if (node_a >= graph->size) {
+        printf("E: node_a index out of range\n");
+        return;
+    }
+    if (node_b >= graph->size) {
+        printf("E: node_b index out of range\n");
+        return;
+    }
+    if (weight == 0) {
+        printf("E: weight cannot be 0\n");
+        return;
+    }
+
+    // create the connection
+    graph->adj_matrix[node_a][node_b] = weight;
+    // since this is an impl of an undirected graph, create the opposite connection
+    graph->adj_matrix[node_b][node_a] = weight;
 }
--- graph/graph.h
+++ graph/graph.h
@@ -4,7 +4,14 @@
 
 typedef struct {
     /**
-     * adjacency matrix
+     * adjacency matrix.
+     *
+     * in an unweighted graph (`weighted` is 0), `adj_matrix[x][y]` is 0
+     * if there is no edge x <-> y, and 1 if there is such an edge.
+     *
+     * in a weighted graph (`weighted` is 1), if an edge x <-> y exists,
+     * `adj_matrix[x][y]` will be equal to the weight of that edge.
+     * if there is no such edge it will be 0.
      */
     int adj_matrix[GRAPH_CAPACITY][GRAPH_CAPACITY];
     /**
@@ -12,7 +19,13 @@ typedef struct {
      */
     char vertex_names[GRAPH_CAPACITY];
     int size;
+    /**
+     * is this graph weighted?
+     */
+    int weighted;
 } Graph;
 
 Graph* create_graph(int size);
+Graph* create_graph_weighted(int size);
 void graph_connect(Graph* graph, int node_a, int node_b);
+void graph_connect_weighted(Graph* graph, int node_a, int node_b, int weight);
