2dc025fb1cf83b9e28d8277e2f2fbf114449effe
githubcatw <17374742+githubcatw@users.noreply.github.com>
2026-04-27 21:49:43 +0400

    lists: add convenience headers
    these headers define LinkedList and ListNode to be that of double or single
    linked list specifically.

---
+++ double_list/linked_is_double.h
@@ -0,0 +1,5 @@
+// this is a convenience header that allows for using "LinkedList"
+// and "ListNode" instead of "DLinkedList" and "DListNode".
+
+#define ListNode DListNode
+#define LinkedList DLinkedList
---
+++ linked_list/linked_is_single.h
@@ -0,0 +1,5 @@
+// this is a convenience header that allows for using "LinkedList"
+// and "ListNode" instead of "SLinkedList" and "SListNode".
+
+#define ListNode SListNode
+#define LinkedList SLinkedList
